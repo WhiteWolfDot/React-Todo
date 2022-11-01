2135ee2dc098cf06f9722ccd66487d828d2e56b2
Adeesh Sharma <adeeshsharma@Adeeshs-MBP.Dlink>
2022-11-01 18:45:43 +0530

    fixed merge conflict

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,7 +4,11 @@ function Footer() {
    return (
       <footer>
          <div id='footer-content'>
+<<<<<<< Updated upstream
             Made with ❤ for
+=======
+            Made for the
+>>>>>>> Stashed changes
             <a id='footer-logo' href='https://github.com/WhiteWolfDot'>
                DevOps Assignment 1 (Git workflow)
             </a>
