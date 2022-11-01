dcc36d34fffb006091d19e6b8dc75a35c8706b88
Adeesh Sharma <adeeshsharma@Adeeshs-MBP.Dlink>
2022-11-01 18:10:45 +0530

    changed application theme and title

--- src/index.js
+++ src/index.js
@@ -1,60 +1,60 @@
-import React, { useEffect, useState } from "react";
-import ReactDOM from "react-dom";
-import Header from "./components/header";
-import TodoForm from "./components/TodoForm";
-import TodoList from "./components/TodoList";
-import Footer from "./components/footer";
-import "./styles/index.css";
+import React, { useEffect, useState } from 'react';
+import ReactDOM from 'react-dom';
+import Header from './components/header';
+import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
+import Footer from './components/footer';
+import './styles/index.css';
 
-const LOCAL_STORAGE_KEY = "react-todo-todos";
+const LOCAL_STORAGE_KEY = 'react-todo-todos';
 
 export default function App() {
-  const [todos, setTodos] = useState([]);
+   const [todos, setTodos] = useState([]);
 
-  useEffect(() => {
-    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
-    if (storedTodos) setTodos(storedTodos);
-  }, []);
+   useEffect(() => {
+      const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+      if (storedTodos) setTodos(storedTodos);
+   }, []);
 
-  useEffect(() => {
-    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
-  }, [todos]);
+   useEffect(() => {
+      localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
+   }, [todos]);
 
-  function addTodo(todo) {
-    setTodos([todo, ...todos]);
-  }
+   function addTodo(todo) {
+      setTodos([todo, ...todos]);
+   }
 
-  function removeItem(id) {
-    setTodos(todos.filter((todo) => todo.id !== id));
-  }
+   function removeItem(id) {
+      setTodos(todos.filter((todo) => todo.id !== id));
+   }
 
-  function toggleComplete(id) {
-    setTodos(
-      todos.map((todo) => {
-        if (todo.id === id) {
-          return { ...todo, completed: !todo.completed };
-        }
-        return todo;
-      })
-    );
-  }
+   function toggleComplete(id) {
+      setTodos(
+         todos.map((todo) => {
+            if (todo.id === id) {
+               return { ...todo, completed: !todo.completed };
+            }
+            return todo;
+         })
+      );
+   }
 
-  return (
-    <>
-      <Header />
-      <div id="todo-app">
-        <h1>React TODO</h1>
-        <TodoForm addTodo={addTodo} />
-        <TodoList
-          todos={todos}
-          toggleComplete={toggleComplete}
-          removeItem={removeItem}
-        />
-        <h4>*Click to expand long TODOs</h4>
-      </div>
-      <Footer />
-    </>
-  );
+   return (
+      <>
+         <Header />
+         <div id='todo-app'>
+            <h1>DevOps Assignment TODO</h1>
+            <TodoForm addTodo={addTodo} />
+            <TodoList
+               todos={todos}
+               toggleComplete={toggleComplete}
+               removeItem={removeItem}
+            />
+            <h4>*Click to expand long TODOs</h4>
+         </div>
+         <Footer />
+      </>
+   );
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'));
