abb3776ccde56642c46629fd742971d6733898ed
adeeshsharma <adeeshon@gmail.com>
2022-11-01 19:07:46 +0530

    footer push

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,11 +4,7 @@ function Footer() {
    return (
       <footer>
          <div id='footer-content'>
-<<<<<<< Updated upstream
-            Made with ❤ for
-=======
             Made for the
->>>>>>> Stashed changes
             <a id='footer-logo' href='https://github.com/WhiteWolfDot'>
                DevOps Assignment 1 (Git workflow)
             </a>
